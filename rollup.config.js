ab7d236547b07236bbb73ed9b62e79bd93e1dc2c
Kiernan McGowan <timmcgowan625@gmail.com>
2017-11-20 08:45:17 -0800

    update package.json

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,11 @@ import npm from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-  entry: 'index.js',
+  input: 'index.js',
   plugins: [npm({jsnext: true}), commonjs({})],
   moduleId: 'd3-nelson-rules',
-  moduleName: 'd3-nelson-rules',
-  format: 'umd'
+  name: 'd3-nelson-rules',
+  output: {
+    format: 'umd'
+  }
 };
